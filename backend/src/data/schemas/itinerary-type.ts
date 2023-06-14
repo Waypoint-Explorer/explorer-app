6b60fb5b5a4c7bdfc92d9e868593d999289f4463
Riccardo Omiccioli <omiccioli.riccardo@gmail.com>
2023-06-14 14:42:17 +0200

    chore: add itinerary schema

---
+++ backend/src/data/schemas/itinerary-type.ts
@@ -0,0 +1,7 @@
+/** Models an itinerary type */
+type ItineraryType = "CULTURAL" | "THEMED" | "NATURALISTIC" | "TOURISTIC" | "FITNESS";
+
+/** An array containing all the possible itinerary types */
+const ItineraryTypeArray: String[] = ["CULTURAL", "THEMED", "NATURALISTIC", "TOURISTIC", "FITNESS"];
+
+export { ItineraryType, ItineraryTypeArray };
---
+++ backend/src/data/schemas/itinerary.ts
@@ -0,0 +1,31 @@
+import mongoose, {Schema} from "mongoose";
+import MongooseTypes = mongoose.Types
+import SchemaTypes = Schema.Types
+import { ItineraryType, ItineraryTypeArray } from "./itinerary-type";
+
+/** Models an itinerary */
+interface Itinerary {
+    /** The id of this itinerary */
+    _id: MongooseTypes.ObjectId,
+    /** The name of this itinerary */
+    name: string,
+    /** The type of this itinerary */
+    type: ItineraryType,
+    /** The description of this itinerary */
+    description?: string,
+    /** The extra points gained completing this itinerary */
+    extra_points: number,
+    /** The waypoints included in the itinerary */
+    waypoints?: MongooseTypes.ObjectId[],
+}
+
+/** Schema for an itinerary */
+const ITINERARY_SCHEMA = new Schema<Itinerary>({
+    name: { type: SchemaTypes.String, required: true },
+    type: { type: SchemaTypes.String, enum: ItineraryTypeArray, required: true },
+    description: { type: SchemaTypes.String, required: false },
+    extra_points: { type: SchemaTypes.Number, required: true },
+    waypoints: { type: Array(SchemaTypes.ObjectId), required: false},
+});
+
+export { Itinerary, ITINERARY_SCHEMA }
