9975624ce2767e4fee881c30337b0bad0092386d
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2023-06-18 18:06:51 +0200

    chore: modify user schema db and add example user data

--- database/init/data/users.js
+++ database/init/data/users.js
@@ -2,9 +2,21 @@ const users = [
   {
     _id: ObjectId("62ab48841c9ec9d5210f45e0"),
     name: "Mario Rossi",
+    email: "mario.rossi@email.com",    //password: "mario"
+    hash: "$2b$10$olHkPDLiaNsvHVzQH0Jg3uS.ASybzmjtEIalb4H4e5nrTs2.ayccW",
+    role: "user",
+    country: "Italia",
+    city: "Roma",
+    birth_year: "1970",
   },
   {
     _id: ObjectId("62ab48841c9ec9d5210f45e1"),
     name: "Giuseppe Verdi",
+    email: "giuseppe.verdi@email.com", //password: "giuseppe"
+    hash: "$2b$10$XiesktXdCuP/VMfB.00Ad.drvz8lJDnNBA.FuekVwsOYy81Hlu8Hq",
+    role: "user",
+    country: "San Marino",
+    city: "Città di San Marino",
+    birth_year: "1981",
   },
 ]
