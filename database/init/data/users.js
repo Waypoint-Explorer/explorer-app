783709f9849185d7b06cbdf975e934ddc45b6fe3
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2023-06-29 18:02:30 +0200

    Revert "chore: add an administrator in db data"
    This reverts commit 33c4bc160c66f1c4d74998e29eff57b85e9cb6a3.

--- database/init/data/users.js
+++ database/init/data/users.js
@@ -19,11 +19,4 @@ const users = [
     city: "Città di San Marino",
     birth_year: "1981",
   },
-  {
-    _id: ObjectId("62ab48841c9ec9d5210f45e6"),
-    name: "Admin 3",
-    email: "admin3@smartparking.com",            //password: "admin3"
-    hash: "$2b$10$42WjtFYNfpzR/D5XDelVHuV2X9i9EzAc85cPdRM2pUnUO9vgQj4Wm",
-    role: "administrator",
-  },
 ]
