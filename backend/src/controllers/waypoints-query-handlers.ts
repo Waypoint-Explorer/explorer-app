1cbf913c1631ed2144129a537884bd0f79223fdb
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2023-07-05 12:15:12 +0200

    chore: add backend request to add a new waypoint

--- backend/src/controllers/waypoints-query-handlers.ts
+++ backend/src/controllers/waypoints-query-handlers.ts
@@ -3,25 +3,47 @@ import {QueryParser} from "../utils/query-parser";
 import {sendError, sendJson} from "./application-handlers";
 import mongoose from "mongoose";
 import Types = mongoose.Types;
-import { ExplorerAppDatabase } from "../data/explorer-app-database";
+import {ExplorerAppDatabase, MarkersDocument, WaypointsDocument} from "../data/explorer-app-database";
+import {Require} from "../utils/require";
+import {StatusCodes} from "http-status-codes";
 
 /**
  * Request handlers that handles queries on waypoints
  */
 export class WaypointsQueryHandlers {
 
-  /** Retrieves all the waypoints */
+  /** Retrieve all the waypoints */
   public static readonly findAll: RequestHandler = (req: Request, res: Response) => {
     ExplorerAppDatabase.Singleton.Waypoints
       .find({}, {}).exec()
       .then(sendJson(req, res), sendError(req, res));
   }
 
-  /** Retrieves a waypoint given the id of the waypoint */
+  /** Retrieve a waypoint given the id of the waypoint */
   public static readonly findById: RequestHandler = (req: Request, res: Response) => {
     ExplorerAppDatabase.Singleton.Waypoints
       .findOne({_id: new Types.ObjectId(req.params.waypointId)}, {}).exec()
       .then(sendJson(req, res), sendError(req, res));
+  }
+
+  /** Insert a new waypoint into the database. */
+  public static readonly insertOne: RequestHandler = (req: Request, res: Response) => {
+    Require.fields(req.body,"name").then(requirementSatisfied => {
+        const newWaypoint = new WaypointsDocument({
+          name: req.body.name,
+        });
+        if (!!req.body.description) { newWaypoint.description = req.body.description; }
+        if (!!req.body.place) { newWaypoint.place = req.body.place; }
+        if (!!req.body.price) { newWaypoint.price = req.body.price; }
+        if (!!req.body.accessibility_info) { newWaypoint.accessibility_info = req.body.accessibility_info; }
+        if (!!req.body.marker) { newWaypoint.marker = new Types.ObjectId(req.body.marker); }
+        ExplorerAppDatabase.Singleton.Waypoints    // Add the requested waypoint to the database
+            .insertMany([newWaypoint]).then(addedWaypoint => {
+            res.status(StatusCodes.OK).json({ addedWaypoint });
+        }, sendError(req, res));
+    }, failure => {
+      res.status(StatusCodes.BAD_REQUEST).send("The request must contain a [name].");
+    });
   }
 
 }
