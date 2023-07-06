e1cb158061c603a6303ccfc03dbb3f4373442ce3
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2023-07-06 15:42:34 +0200

    chore: add management of opening hours and days for new waypoint

--- frontend/src/pages/WaypointPage.vue
+++ frontend/src/pages/WaypointPage.vue
@@ -1,157 +1,219 @@
 <script lang="ts">
-import {defineComponent} from "vue";
-import axios, {AxiosError} from "axios";
-import {Environment} from "../environment";
+  import {defineComponent} from "vue";
+  import axios, {AxiosError} from "axios";
+  import {Environment} from "../environment";
 
-export default defineComponent({
-  data(){
-    return {
-      showMenu: false,
-      success: false,
-      name: "",
-      description: "",
-      place: "",
-      accessibility_info: "",
-      price: {
+  export default defineComponent({
+    data(){
+      return {
+        showMenu: false,
+        success: false,
+        newSlot: false,
+        name: "",
         description: "",
-        price: ""
+        place: "",
+        accessibility_info: "",
+        price: {
+          description: "",
+          price: 0
+        },
+        selected_day: "",
+        opening_hour: "",
+        closing_hour: "",
+        closed_days: null,
+        days: [
+          { name: 'Lunedì', code: 'lun' },
+          { name: 'Martedì', code: 'mar' },
+          { name: 'Mercoledì', code: 'mer' },
+          { name: 'Giovedì', code: 'gio' },
+          { name: 'Venerdì', code: 'ven' },
+          { name: 'Sabato', code: 'sab' },
+          { name: 'Domenica', code: 'dom' },
+        ],
+        opening_hours: {
+          monday_hours: [],
+          tuesday_hours: [],
+          wednesday_hours: [],
+          thursday_hours: [],
+          friday_hours: [],
+          saturday_hours: [],
+          sunday_hours: [],
+        },
+        markers: [],
+        selectedMarker: "",
+        waypoints: [],
+        formError: {
+          cause: "",
+          message: "",
+        },
+      }
+    },
+    mounted(){
+      this.allWaypoints();
+    },
+    methods:{
+      allMarkers(){
+        axios.get(`http://${Environment.BACKEND_HOST}/markers`)
+            .then((response) => {
+              this.showMenu=true;
+              this.markers = [];
+              response.data.forEach((marker) =>{
+                this.markers.push({
+                  _id: `${marker._id}`,
+                  name: `${marker.marker_id}, ${marker.type}`,
+                  id: `${marker.marker_id}`,
+                  coordinates: {
+                    latitude: `${marker.coordinates.latitude}`,
+                    longitude: `${marker.coordinates.longitude}`,
+                  },
+                  type: `${marker.type}`,
+                  points: `${marker.points}`,
+                });
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
       },
-      markers: [],
-      selectedMarker: "",
-      waypoints: [],
-      formError: {
-        cause: "",
-        message: "",
+      allWaypoints(){
+        axios.get(`http://${Environment.BACKEND_HOST}/waypoints`)
+            .then((response) => {
+              this.waypoints = [];
+              response.data.forEach((waypoint) =>{
+                let place = !!waypoint.place ? waypoint.place : "";
+                let markerId = !!waypoint.marker ? waypoint.marker : "";
+
+                this.waypoints.push({
+                  name: `${waypoint.name}`,
+                  place: `${place}`,
+                  marker: `${markerId}`,
+                });
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+      },
+      addWaypoint(){
+        if(this.checkForm()){
+          this.addTimeSlot(false);
+          let newWaypoint: any = {
+            name: this.name,
+            marker: this.selectedMarker['_id'],
+          };
+          if (this.description!=="") { newWaypoint.description = this.description; }
+          if (this.place!=="") { newWaypoint.place = this.place; }
+          if (this.price.description!=="" && this.price.price!=="") { newWaypoint.price = this.price; }
+          if (this.accessibility_info!=="") { newWaypoint.accessibility_info = this.accessibility_info; }
+          if (this.opening_hours!==null) {newWaypoint.opening_hours = this.opening_hours;}
+          if (this.closed_days!==null) {newWaypoint.opening_hours.closing_days = this.closed_days;}
+          console.log(JSON.stringify(this.opening_hours));
+          axios.post(`http://${Environment.BACKEND_HOST}/waypoints`, newWaypoint)
+              .then(() => {
+                this.showMenu=false;
+                this.allWaypoints();
+                this.resetForm();
+                this.success = true;
+              })
+              .catch(this.displayError);
+        }
+      },
+      checkForm() : boolean {
+        if (this.name === "") {
+          this.formError.cause = "name";
+          this.formError.message = "Il nome della tappa è richiesto per l'inserimento.";
+        } else if(this.selectedMarker === ""){
+          this.formError.cause = "marker";
+          this.formError.message = "Il marcatore associato è richiesto per l'inserimento.";
+        } else if((this.selected_day!=='') && (this.opening_hour!=='' && this.closing_hour==='')){
+          this.formError.cause = "closing_hour";
+          this.formError.message = "Inserire l'orario di chiusura";
+        } else if((this.selected_day!=='') && (this.opening_hour==='' && this.closing_hour!=='')){
+          this.formError.cause = "opening_hour";
+          this.formError.message = "Inserire l'orario di apertura";
+        } else if((this.selected_day!=='') && (this.opening_hour==='' && this.closing_hour==='')){
+          this.formError.cause = "hours";
+          this.formError.message = "Inserire gli orari";
+        }
+        else {
+          return true;
+        }
+        return false;
+      },
+      addTimeSlot(newSlot){
+        this.newSlot = newSlot;
+        if(this.opening_hour!=='' && this.closing_hour!==''){
+          switch (this.selected_day['code']) {
+            case "lun": {
+              this.opening_hours.monday_hours.push({shift_start: Number(this.opening_hour.replace(":","")), shift_end: Number(this.closing_hour.replace(":",""))});
+              break;
+            }
+            case "mar": {
+              this.opening_hours.tuesday_hours.push({shift_start: Number(this.opening_hour.replace(":","")), shift_end: Number(this.closing_hour.replace(":",""))});
+              break;
+            }
+            case "mer": {
+              this.opening_hours.wednesday_hours.push({shift_start: Number(this.opening_hour.replace(":","")), shift_end: Number(this.closing_hour.replace(":",""))});
+              break;
+            }
+            case "gio": {
+              this.opening_hours.thursday_hours.push({shift_start: Number(this.opening_hour.replace(":","")), shift_end: Number(this.closing_hour.replace(":",""))});
+              break;
+            }
+            case "ven": {
+              this.opening_hours.friday_hours.push({shift_start: Number(this.opening_hour.replace(":","")), shift_end: Number(this.closing_hour.replace(":",""))});
+              break;
+            }
+            case "sab": {
+              this.opening_hours.saturday_hours.push({shift_start: Number(this.opening_hour.replace(":","")), shift_end: Number(this.closing_hour.replace(":",""))});
+              break;
+            }
+            case "dom": {
+              this.opening_hours.sunday_hours.push({shift_start: Number(this.opening_hour.replace(":","")), shift_end: Number(this.closing_hour.replace(":",""))});
+              break;
+            }
+          }
+          this.opening_hour="";
+          this.closing_hour="";
+          this.selected_day="";
+        }else{
+          this.formError.cause = "hours";
+          this.formError.message = "Inserire gli orari";
+        }
+      },
+      displayError(error: AxiosError) {
+        console.log("Add waypoint error: " + error.response + "\tMessage: " + error.response?.data);
+      },
+      resetForm(){
+        this.markers=[];
+        this.selectedMarker="";
+        this.name="";
+        this.description="";
+        this.place="";
+        this.accessibility_info="";
+        this.selected_day= "";
+        this.opening_hour= "";
+        this.closing_hour= "";
+        this.closed_days= null;
+        this.opening_hours= {
+          monday_hours: [],
+          tuesday_hours: [],
+          wednesday_hours: [],
+          thursday_hours: [],
+          friday_hours: [],
+          saturday_hours: [],
+          sunday_hours: [],
+        };
+        this.price= {
+          description: "",
+          price: 0
+        };
+        this.formError.cause = "";
+        this.formError.message = "";
+
       },
     }
-  },
-  mounted(){
-    this.allWaypoints();
-  },
-  methods:{
-    allMarkers(){
-      axios.get(`http://${Environment.BACKEND_HOST}/markers`)
-          .then((response) => {
-            this.showMenu=true;
-            this.markers = [];
-            response.data.forEach((marker) =>{
-              this.markers.push({
-                _id: `${marker._id}`,
-                name: `${marker.marker_id}, ${marker.type}`,
-                id: `${marker.marker_id}`,
-                coordinates: {
-                  latitude: `${marker.coordinates.latitude}`,
-                  longitude: `${marker.coordinates.longitude}`,
-                },
-                type: `${marker.type}`,
-                points: `${marker.points}`,
-              });
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-    },
-    allWaypoints(){
-      axios.get(`http://${Environment.BACKEND_HOST}/waypoints`)
-          .then((response) => {
-            this.waypoints = [];
-            response.data.forEach((waypoint) =>{
-              let description = !!waypoint.description ? waypoint.description : "";
-              let place = !!waypoint.place ? waypoint.place : "";
-              let priceDescription = !!waypoint.price ? waypoint.price.description : "";
-              let priceValue = !!waypoint.price ? waypoint.price.price : "";
-              let accessibility = !!waypoint.accessibility_info ? waypoint.accessibility_info : "";
-              let markerId = !!waypoint.marker ? waypoint.marker : "";
-
-              this.waypoints.push({
-                name: `${waypoint.name}`,
-                description: `${description}`,
-                place: `${place}`,
-                price:{
-                  description: `${priceDescription}`,
-                  price: `${priceValue}`,
-                },
-                accessibility_info: `${accessibility}`,
-                marker: `${markerId}`,
-                opening_hours: !!waypoint.opening_hours ? this.formatOpeningHours(JSON.stringify(waypoint.opening_hours)) : "",
-              });
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-    },
-    addWaypoint(){
-      if(this.checkForm()){
-        let newWaypoint: any = {
-          name: this.name,
-          marker: this.selectedMarker['_id'],
-        };
-        if (this.description!=="") { newWaypoint.description = this.description; }
-        if (this.place!=="") { newWaypoint.place = this.place; }
-        if (this.price.description!=="" && this.price.price!=="") { newWaypoint.price = this.price; }
-        if (this.accessibility_info!=="") { newWaypoint.accessibility_info = this.accessibility_info; }
-        axios.post(`http://${Environment.BACKEND_HOST}/waypoints`, newWaypoint)
-            .then(() => {
-              this.showMenu=false;
-              this.allWaypoints();
-              this.resetForm();
-              this.success = true;
-            })
-            .catch(this.displayError);
-      }
-    },
-    checkForm() : boolean {
-      if (this.name === "") {
-        this.formError.cause = "name";
-        this.formError.message = "Il nome della tappa è richiesto per l'inserimento.";
-      } else if(this.selectedMarker === ""){
-        this.formError.cause = "marker";
-        this.formError.message = "Il marcatore associato è richiesto per l'inserimento.";
-      } else {
-        return true;
-      }
-      return false;
-    },
-    displayError(error: AxiosError) {
-      console.log("Add waypoint error: " + error.response + "\tMessage: " + error.response?.data);
-    },
-    resetForm(){
-      this.markers=[];
-      this.name="";
-      this.description="";
-      this.place="";
-      this.accessibility_info="";
-      this.price= {
-        description: "",
-        price: ""
-      };
-      this.selectedMarker="";
-    },
-    formatOpeningHours(openingHours){
-      let data = openingHours.replaceAll("],"," | ");
-      data = data.replaceAll("{","");
-      data = data.replaceAll("\"","");
-      data = data.replaceAll("}"," ");
-      data = data.replaceAll("["," ");
-      data = data.replaceAll("monday_hours","lunedì");
-      data = data.replaceAll("tuesday_hours","martedì");
-      data = data.replaceAll("wednesday_hours","mercoledì");
-      data = data.replaceAll("thursday_hours","giovedì");
-      data = data.replaceAll("friday_hours","venerdì");
-      data = data.replaceAll("saturday_hours","sabato");
-      data = data.replaceAll("sunday_hours","domenica");
-      data = data.replaceAll("closing_days","giorni chiusura");
-      data = data.replaceAll("shift_start:","dalle ");
-      data = data.replaceAll("shift_end:","alle ");
-      data = data.replaceAll(","," ");
-      data = data.replaceAll(":","=>");
-      data = data.replaceAll(" ]"," no");
-      return data;
-    },
-  }
-});
+  });
 </script>
 
 <template>
@@ -180,7 +242,7 @@ export default defineComponent({
     </div>
 
     <div class="p-field">
-      <label for="latitude">Luogo</label>
+      <label for="place">Luogo</label>
       <inputTextComp id="place" v-model="place" type="text" placeholder="Luogo" :class="{'p-invalid': formError.cause === 'place'}"/>
       <small v-if="formError.cause === 'place'" class="p-error">{{ this.formError.message }}</small>
     </div>
@@ -190,8 +252,7 @@ export default defineComponent({
       <inputTextComp id="price" v-model="price.description" type="text" placeholder="Descrizione" :class="{'p-invalid': formError.cause === 'price'}"/>
       <small v-if="formError.cause === 'price'" class="p-error">{{ this.formError.message }}</small>
 
-      <inputTextComp id="price" v-model="price.price" type="text" placeholder="Costo €" :class="{'p-invalid': formError.cause === 'price'}"/>
-      <small v-if="formError.cause === 'price'" class="p-error">{{ this.formError.message }}</small>
+      <inputNumberComp id="price" v-model="price.price" placeholder="Costo €" mode="currency" currency="EUR"/>
     </div>
 
     <div class="p-field">
@@ -200,18 +261,64 @@ export default defineComponent({
       <small v-if="formError.cause === 'accessibility'" class="p-error">{{ this.formError.message }}</small>
     </div>
 
+    <div class="p-field">
+      <label for="closed_days">Giorni di chiusura</label>
+      <chipsComp id="closed_days" v-model="closed_days" separator="," />
+    </div>
+
+    <div class="p-field">
+      <label for="opening">Giorno di apertura</label>
+      <dropdownComp id="opening" v-model="selected_day" :options="days" optionLabel="name" placeholder="Giorno di apertura"/>
+
+      <div v-if="selected_day" style="display:flex;justify-content: space-between;align-items: center">
+        <div style="display: flex;flex-direction: column">
+          <label v-if="selected_day" for="opening_hour">Dalle</label>
+          <inputMaskComp id="opening_hour" v-model="opening_hour" mask="99:99" placeholder="99:99" style="width:6rem" :class="{'p-invalid': (formError.cause === 'opening_hour') || (formError.cause === 'hours')}"/>
+        </div>
+
+        <div style="display: flex;flex-direction: column">
+          <label v-if="selected_day" for="closing_hour">Alle</label>
+          <inputMaskComp id="closing_hour" v-model="closing_hour" mask="99:99" placeholder="99:99" style="width:6rem" :class="{'p-invalid': (formError.cause === 'closing_hour') || (formError.cause === 'hours')}"/>
+        </div>
+
+        <buttonComp class="confirm-button" icon="add" rounded raised @click.prevent="addTimeSlot(true)" style="padding:0;width:2.488rem;margin:0">
+          <span class="material-icons">add</span>
+        </buttonComp>
+      </div>
+      <small v-if="selected_day && (formError.cause === 'closing_hour' || formError.cause === 'opening_hour' || formError.cause === 'hours')" class="p-error">{{ this.formError.message }}</small>
+    </div>
+
+    <div class="p-field" v-if="newSlot">
+      <label v-if="opening_hours.monday_hours.length>0" for="monday_time_slot">Lunedì</label>
+      <chipsComp v-if="opening_hours.monday_hours.length>0" id="monday_time_slot" v-model="opening_hours.monday_hours" separator="," />
+
+      <label v-if="opening_hours.tuesday_hours.length>0" for="tuesday_time_slot">Martedì</label>
+      <chipsComp v-if="opening_hours.tuesday_hours.length>0" id="tuesday_time_slot" v-model="opening_hours.tuesday_hours" separator="," />
+
+      <label v-if="opening_hours.wednesday_hours.length>0" for="wednesday_time_slot">Mercoledì</label>
+      <chipsComp v-if="opening_hours.wednesday_hours.length>0" id="wednesday_time_slot" v-model="opening_hours.wednesday_hours" separator="," />
+
+      <label v-if="opening_hours.thursday_hours.length>0" for="thursday_time_slot">Giovedì</label>
+      <chipsComp v-if="opening_hours.thursday_hours.length>0" id="thursday_time_slot" v-model="opening_hours.thursday_hours" separator="," />
+
+      <label v-if="opening_hours.friday_hours.length>0" for="friday_time_slot">Venerdì</label>
+      <chipsComp v-if="opening_hours.friday_hours.length>0" id="friday_time_slot" v-model="opening_hours.friday_hours" separator="," />
+
+      <label v-if="opening_hours.saturday_hours.length>0" for="saturday_time_slot">Sabato</label>
+      <chipsComp v-if="opening_hours.saturday_hours.length>0" id="saturday_time_slot" v-model="opening_hours.saturday_hours" separator="," />
+
+      <label v-if="opening_hours.sunday_hours.length>0" for="sunday_time_slot">Domenica</label>
+      <chipsComp v-if="opening_hours.sunday_hours.length>0" id="sunday_time_slot" v-model="opening_hours.sunday_hours" separator="," />
+    </div>
+
     <buttonComp class="confirm-button" type="button" label="Aggiungi" @click.prevent="addWaypoint"/>
   </div>
 
   <div class="card">
     <cardComp v-for="waypoint in this.waypoints">
       <template #title> {{ waypoint.name }} </template>
-      <template #subtitle v-if="waypoint.description!==''"> {{ waypoint.description }}</template>
       <template #content>
         <p v-if="waypoint.place!==''">luogo: {{waypoint.place}}</p>
-        <p v-if="waypoint.price.description!=='' && waypoint.price.price!==''">prezzo: {{waypoint.price.description}} {{waypoint.price.price}}</p>
-        <p v-if="waypoint.opening_hours!==''">orari di apertura: {{waypoint.opening_hours}}</p>
-        <p v-if="waypoint.accessibility_info!==''">accessibilità: {{waypoint.accessibility_info}}</p>
         <p v-if="waypoint.marker!==''">marcatore: {{waypoint.marker}}</p>
       </template>
 
