96f3fb6494a6c2aace364e8ea349ae012f84ace3
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2023-07-07 19:21:13 +0200

    chore: add API query for request of adding a new itinerary

--- backend/src/controllers/itineraries-query-handlers.ts
+++ backend/src/controllers/itineraries-query-handlers.ts
@@ -1,11 +1,11 @@
 import {Request, RequestHandler, Response} from "express";
-import {QueryParser} from "../utils/query-parser";
 import {sendError, sendJson} from "./application-handlers";
 import mongoose from "mongoose";
 import Types = mongoose.Types;
-import { ExplorerAppDatabase } from "../data/explorer-app-database";
+import {ExplorerAppDatabase, ItinerariesDocument} from "../data/explorer-app-database";
 import {StatusCodes} from "http-status-codes";
 import {ItineraryTypeArray} from "../data/schemas/itinerary-type";
+import {Require} from "../utils/require";
 
 /**
  * Request handlers that handles queries on itineraries
@@ -36,5 +36,26 @@ export class ItinerariesQueryHandlers {
     }
   }
 
-
+  /** Insert a new itinerary into the database. */
+  public static readonly insertOne: RequestHandler = (req: Request, res: Response) => {
+    Require.fields(req.body,"name", "type", "extra_points").then(requirementSatisfied => {
+      const newItinerary = new ItinerariesDocument({
+        name: req.body.name,
+        type: req.body.type,
+        extra_points: req.body.extra_points,
+      });
+      if (!!req.body.description) { newItinerary.description = req.body.description; }
+      if (!!req.body.waypoints) {
+        let waypointsList: any[] = [];
+        req.body.waypoints.forEach((t: any) => waypointsList.push(new Types.ObjectId(t.id)));
+        newItinerary.waypoints = waypointsList;
+      }
+      ExplorerAppDatabase.Singleton.Itineraries    // Add the requested itinerary to the database
+          .insertMany([newItinerary]).then(addedItinerary => {
+        res.status(StatusCodes.OK).json({ addedItinerary });
+      }, sendError(req, res));
+    }, failure => {
+      res.status(StatusCodes.BAD_REQUEST).send("The request must contain a [name], [type] and [extra_points].");
+    });
+  }
 }
