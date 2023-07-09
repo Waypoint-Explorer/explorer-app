232037c67ad7ce13982b2147fd11d66bf4b250e0
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2023-07-10 00:40:48 +0200

    chore: add chart, filter according to period and backend request for all completed itineraries

--- frontend/src/pages/StatisticsPage.vue
+++ frontend/src/pages/StatisticsPage.vue
@@ -6,36 +6,131 @@
   export default defineComponent({
     data(){
       return {
-        filter: false,
+        filtered: false,
         completedItineraries: [],
         periods: [
-          {name:"Ultima settimana", code:"sett"},
-          {name:"Ultimo mese", code:"mese"},
-          {name:"Ultimo anno", code:"anno"},
+          {name:"Ultima settimana", code:"week"},
+          {name:"Ultimo mese", code:"month"},
+          {name:"Ultimo anno", code:"year"},
         ],
         selectedPeriod: "",
+        horizontalOptions: {
+          plugins: {
+            legend: {
+              labels: {
+                color: "#2c3e50",
+              },
+            },
+          },
+          scales: {
+            x: {
+              ticks: {
+                color: "#495057",
+              },
+              grid: {
+                color: "#ebedef",
+              },
+            },
+            y: {
+              ticks: {
+                color: "#495057",
+              },
+              grid: {
+                color: "#ebedef",
+              },
+            },
+          },
+        },
+        basicData: {
+          labels: [],
+          datasets: [
+            {
+              label: "Numero percorsi",
+              backgroundColor: "#FFA726",
+              data: [],
+            },
+          ],
+        },
+        numCompletedItinerariesPeriod: 0,
+        itinerariesPeriod: [],
+        chartData: [],
       }
     },
     mounted() {
-      //this.allCompletedItineraries();
+      this.allCompletedItineraries();
     },
     methods: {
       allCompletedItineraries(){
         axios.get(`http://${Environment.BACKEND_HOST}/completed-itineraries`)
             .then((response) => {
               this.completedItineraries = [];
-
+              console.log(JSON.stringify(response.data));
               response.data.forEach((itinerary) =>{
+                let related_itinerary = !!itinerary.related_itinerary ? itinerary.related_itinerary : "";
+                let visited_waypoints = !!itinerary.visited_waypoints ? itinerary.visited_waypoints : "";
+                let points_earned = !!itinerary.points_earned ? itinerary.points_earned : "";
+                let completion_date = !!itinerary.completion_date ? new Date(itinerary.completion_date) : "";
+
                 this.completedItineraries.push({
-                  related_itinerary: `${itinerary.related_itinerary}`,
-                  start_date: `${itinerary.start_date}`,
-                  completion_date: `${itinerary.completion_date}`,
+                  related_itinerary: `${related_itinerary}`,
+                  visited_waypoints: `${visited_waypoints}`,
+                  points_earned: `${points_earned}`,
+                  start_date: new Date(itinerary.start_date),
+                  completion_date: `${completion_date}`,
                 });
               });
             })
             .catch((error) => {
               console.log(error);
             });
+      },
+      filter(){
+        if (this.selectedPeriod != null) {
+          this.basicData.datasets.data = [];
+          this.basicData.labels = [];
+          this.numCompletedItinerariesPeriod = 0;
+          this.filtered = true;
+
+          switch (this.selectedPeriod['code']){
+            case "week":
+              this.setChartData(7);
+              break;
+            case "month":
+              this.setChartData(30);
+              break;
+            case "year":
+              this.setChartData(365);
+              break;
+          }
+        }
+      },
+      setChartData(offset){
+        var i: number;
+        let today = new Date(Date.now());
+        this.chartData = [];
+        let offestDate = new Date();
+        offestDate.setDate(today.getDate()-offset);
+
+        this.completedItineraries.forEach((itinerary: any)=>{
+          if(itinerary.start_date.toISOString() >= offestDate.toISOString() || itinerary.start_date.toISOString() <= today.toISOString()){
+            this.numCompletedItinerariesPeriod+=1;
+            this.itinerariesPeriod.push(itinerary);
+          }
+        });
+
+        for (i = offset; i >= 0; i--) {
+          let date = new Date();
+          date.setDate(today.getDate()-i);
+          this.chartData.push({
+            label: date.toLocaleDateString(),
+            visits: 0
+          });
+        }
+
+        this.chartData.forEach((l) => {
+          this.basicData.labels.push(l.label);
+          this.basicData.datasets.data.push(l.visits);
+        });
       },
     },
   });
@@ -45,18 +140,15 @@
   <h1>Statistiche</h1>
   <h2>Percorsi effettuati</h2>
 
-  <div v-if="completedItineraries.length>0" style="padding: 1rem">
+  <div v-if="completedItineraries.length>0">
     <div class="filter">
-      <dropdownComp v-model="selectedPeriod" :options="periods"
-          optionLabel="name"
-          optionValue="code"
-          placeholder="Seleziona Periodo"
-      />
-      <buttonComp type="button" icon="pi pi-search" label="Filtra" @click.prevent="this.filter=true"/>
+      <dropdownComp v-model="selectedPeriod" :options="periods" optionLabel="name" placeholder="Seleziona Periodo"/>
+      <buttonComp type="button" icon="pi pi-search" label="Filtra" @click.prevent="filter" style="margin-top:1rem"/>
     </div>
 
     <div class="stats" v-if="filter">
-      <h3 class="subtitle">Dati Relativi a </h3>
+      <h3 class="subtitle">Dati Relativi a "{{this.selectedPeriod['name']}}"</h3>
+      <p>Numero totale di percorsi effettuati: {{this.numCompletedItinerariesPeriod}}</p>
       <div class="p-chart">
         <chartComp chartComp="margin: 5px 0px 5px 0px" type="bar" :data="basicData" :options="horizontalOptions"/>
       </div>
@@ -72,5 +164,8 @@
   .error{
     text-align: center;
     font-style: italic;
+  }
+  .stats{
+    margin-bottom: 5rem;
   }
 </style>
